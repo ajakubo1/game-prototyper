0f682dd614607de6a48698a33e172dfb2ae3de4f
Adam Jakubowski <ajakubo1@gmail.com>
2016-02-27 18:05:19 +0100

    Added samples and corrected minor issues

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # game-prototyper
 Little library which allows me to create canvas game prototypes (squares, circles, texts, all of that ;))
+
+Right now it only supports drawing squares, I will be probably extending that when I need new functionalities.
+
+## API
+
+- ``defaultDrawConfig`` - a set of default values (config)
+- ``drawRect`` - draws rectangle on provided 2d context with provided config
+- ``generateCanvas`` - generates canvas with provided width and height
+- ``generateRectCanvas`` - generates canvas with drawn rectangle
+
+For function parameters and default parameters, browse source code
+
+## From the author
+
+If you have any suggestions for the library, write me an email (ajakubo1@gmail.com) or open new issue on github!
--- src/prototyper2d.js
+++ src/prototyper2d.js
@@ -5,16 +5,14 @@
 
 var PROTOTYPER = {};
 
-PROTOTYPER.prototype.defaultDrawValues = function () {
-    return {
-        x: 0,
-        y: 0,
-        width: 10,
-        height: 10,
-        lineWidth: 1,
-        strokeStyle: "blue",
-        fillStyle: "red"
-    };
+PROTOTYPER.defaultDrawConfig = {
+    x: 0,
+    y: 0,
+    width: 10,
+    height: 10,
+    lineWidth: 1,
+    strokeStyle: "blue",
+    fillStyle: "red"
 };
 
 /**
@@ -29,7 +27,8 @@ PROTOTYPER.prototype.defaultDrawValues = function () {
  * @param config.strokeStyle
  * @param config.fillStyle
  */
-PROTOTYPER.prototype.drawRect = function (context, config) {
+PROTOTYPER.drawRect = function (context, config) {
+    config = config || this.defaultDrawConfig;
     context.beginPath();
     context.rect(config.x, config.y, config.width, config.height);
     context.closePath();
@@ -46,7 +45,7 @@ PROTOTYPER.prototype.drawRect = function (context, config) {
  * @param height
  * @returns {Element}
  */
-PROTOTYPER.prototype.generateCanvas = function (width, height) {
+PROTOTYPER.generateCanvas = function (width, height) {
     var canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
@@ -55,19 +54,22 @@ PROTOTYPER.prototype.generateCanvas = function (width, height) {
 
 /**
  *
- * @param width
- * @param height
- * @param config
- * @param config.x
- * @param config.y
- * @param config.width
- * @param config.height
- * @param config.lineWidth
- * @param config.strokeStyle
- * @param config.fillStyle
+ * @param [width = PROTOTYPER.defaultDrawConfig.width]
+ * @param [height = PROTOTYPER.defaultDrawConfig.height]
+ * @param [config = PROTOTYPER.defaultDrawConfig]
+ * @param [config.x = 0]
+ * @param [config.y = 0]
+ * @param [config.width = 10]
+ * @param [config.height = 10]
+ * @param [config.lineWidth = 1]
+ * @param [config.strokeStyle = "blue"]
+ * @param [config.fillStyle = "red"]
  * @returns {Element}
  */
-PROTOTYPER.prototype.generateRectCanvas = function (width, height, config) {
+PROTOTYPER.generateRectCanvas = function (width, height, config) {
+    config = config || this.defaultDrawConfig;
+    width = width || this.defaultDrawConfig.width;
+    height = height || this.defaultDrawConfig.height;
     var canvas = this.generateCanvas(width, height);
     this.drawRect(canvas.getContext('2d'), config);
     return canvas;
